fb17eaf7c34cbcb1ad7f2adf11347459fd09944d
“Jesus <jesusdsaravia@gmail.com>
2016-08-30 11:36:04 -0500

    clase persona terminada

---
+++ src/clases/Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clases;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jsaravia1
+ */
+public class Test {
+    public static void main(String[] args) {
+       Persona p;
+       p = new Persona (98122212,"Jesús","Saravia");
+      Persona  q = new Persona (225516550,"Lisse","Barandica");
+        JOptionPane.showMessageDialog(null,"el nombre del objeto p es: "+ p.getPrimer_nombre());
+    }
+}
